1a9d1451398d3f42f78c3e8f543c577d2244e5ea
AndreyVZav <zav.av@bk.ru>
2025-03-24 15:56:11 +0300

    confirmationDialog()

---
+++ Instafilter/ContentView2.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView2.swift
+//  Instafilter
+//
+//  Created by Андрей Завадский on 24.03.2025.
+//
+import SwiftUI
+
+struct ContentView2: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+
+    var body: some View {
+        Button("Hello, World!") {
+            showingConfirmation = true
+        }
+        .frame(width: 300, height: 300)
+        .background(backgroundColor)
+        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+            Button("Red") { backgroundColor = .red }
+            Button("Green") { backgroundColor = .green }
+            Button("Blue") { backgroundColor = .blue }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Select a new color")
+        }
+    }
+}
+
+#Preview {
+    ContentView2()
+}
